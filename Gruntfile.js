76163e2b084e68031fc4cbf5f07e1a4abaf6dcb6
Phil Hughes <phil.hughes@yoma.co.uk>
2015-10-21 09:39:49 +0100

    initial commit :star:

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    browserify: {
+      dist: {
+        files: {
+          'dist/trello-card.js': ['lib/trello-card.js']
+        },
+        options: {
+          transform: ['babelify', 'stringify']
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'dist/trello-card.css': 'lib/trello-card.scss'
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['lib/*.js', 'lib/*.html'],
+        tasks: ['browserify']
+      },
+      sass: {
+        files: ['lib/*.scss'],
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+
+  grunt.registerTask('default', ['watch']);
+};
